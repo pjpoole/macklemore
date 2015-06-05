5e64ff9103bae25d026ee0bc1ffc197435e3a3e1
Peter Poole <peter.j.poole@gmail.com>
2015-06-04 21:33:06 -0400

    Move converter out of loop

--- app/utils/csv_importer.rb
+++ app/utils/csv_importer.rb
@@ -19,24 +19,24 @@ def try(headers = true)
 
   csv_files = Dir.glob(data_directory)
 
-  csv_files.each do |csv_file|
-    converter = lambda do |entry, ary|
-      entry.gsub!(/(\n|\r)/,'')
-      case ary.header
-      when :date
-        # TODO: I18n of date parsing
-        Date.strptime(entry, "%m/%d/%Y")
-      when :debit, :credit
-        entry.gsub!(/,/,'').to_f
-      when :description
-        entry.strip!
-      else
-        entry
-      end
-
+  converter = lambda do |entry, ary|
+    entry.gsub!(/(\n|\r)/,'')
+    case ary.header
+    when :date
+      # TODO: I18n of date parsing
+      Date.strptime(entry, "%m/%d/%Y")
+    when :debit, :credit
+      entry.gsub!(/,/,'').to_f
+    when :description
+      entry.strip!
+    else
       entry
     end
 
+    entry
+  end
+
+  csv_files.each do |csv_file|
     file = CSVFile.open(csv_file, "r")
     CSV.new(
       file,
